1201f2b11f343f67d1b0aac3788233c616b62bbf
Netanel Vaknin <emailofnetanel@gmail.com>
2021-05-15 00:08:00 +0300

    Move all routes logic to controllers folder

---
+++ server/controllers/posts.js
@@ -0,0 +1,61 @@
+const Post = require("../models/posts");
+
+const addPost = async (req, res) => {
+    const post = req.body;
+  
+    try {
+      const doc = await Post.create(post);
+      if (doc) {
+        res.status(200).send("Success!");
+      }
+    } catch (e) {
+      res.status(400).send("Something is wrong with your request");
+    }
+  };
+
+
+const deletePost = async (req, res) => {
+    try {
+      const doc = await Post.findOneAndDelete({ _id: req.body._id });
+      if (doc) {
+        res.status(200).send("Success!");
+      } else {
+        res.status(404).send(`Cant find user with id: ${req.body._id}`);
+      }
+    } catch {
+      res.status(400).send("Please provide _id");
+    }
+  };
+
+const updatePost = async (req, res) => {
+    try {
+      const doc = await Post.findOneAndUpdate(
+        { _id: req.body._id },
+        { ...req.body }
+      );
+  
+      if (doc) {
+        res.status(200).send("Success!");
+      } else {
+        res.status(404).send(`Cant find user with id : ${req.body._id}`);
+      }
+    } catch {
+      res.status(400).send("Please provide _id");
+    }
+  };
+
+const getPosts = async (req, res) => {
+    try {
+      const posts = await Post.find();
+      res.status(200).send(posts);
+    } catch {
+      res.status(400).send("Something went wrong");
+    }
+  };
+
+module.exports = {
+    addPost,
+    deletePost,
+    updatePost,
+    getPosts
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,57 +1,10 @@
 const router = require("express").Router();
-const Post = require("../models/posts");
 const authenticateToken = require("../middlewares/authentication");
+const {addPost, deletePost, updatePost, getPosts} = require('../controllers/posts');
 
-router.post("/posts/insert", authenticateToken, async (req, res) => {
-  const post = req.body;
-
-  try {
-    const doc = await Post.create(post);
-    if (doc) {
-      res.status(200).send("Success!");
-    }
-  } catch (e) {
-    res.status(400).send("Something is wrong with your request");
-  }
-});
-
-router.post("/posts/delete", authenticateToken, async (req, res) => {
-  try {
-    const doc = await Post.findOneAndDelete({ _id: req.body._id });
-    if (doc) {
-      res.status(200).send("Success!");
-    } else {
-      res.status(404).send(`Cant find user with id: ${req.body._id}`);
-    }
-  } catch {
-    res.status(400).send("Please provide _id");
-  }
-});
-
-router.post("/posts/update", authenticateToken, async (req, res) => {
-  try {
-    const doc = await Post.findOneAndUpdate(
-      { _id: req.body._id },
-      { ...req.body }
-    );
-
-    if (doc) {
-      res.status(200).send("Success!");
-    } else {
-      res.status(404).send(`Cant find user with id : ${req.body._id}`);
-    }
-  } catch {
-    res.status(400).send("Please provide _id");
-  }
-});
-
-router.get("/posts", async (req, res) => {
-  try {
-    const posts = await Post.find();
-    res.status(200).send(posts);
-  } catch {
-    res.status(400).send("Something went wrong");
-  }
-});
+router.post("/posts/insert", authenticateToken, addPost);
+router.post("/posts/delete", authenticateToken, deletePost);
+router.post("/posts/update", authenticateToken, updatePost);
+router.get("/posts", getPosts);
 
 module.exports = router;
