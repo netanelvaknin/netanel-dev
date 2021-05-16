7c3a7f5f019022d33533a3073383b14b5b81b2e2
Netanel Vaknin <emailofnetanel@gmail.com>
2021-05-16 17:49:26 +0300

    Router fix

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(userRoute);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
 }
 
 app.listen(PORT, () => {
