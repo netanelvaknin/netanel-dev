0bae60ca1fd494495ae13d9908cd265c2b34ebe6
Netanel Vaknin <emailofnetanel@gmail.com>
2021-05-16 16:39:53 +0300

    Change directories

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 require("./db/dbcon");
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-const PORT = 5000;
 const postsRoute = require("./routes/posts");
 const adminRoute = require("./routes/admin");
 const userRoute = require("./routes/users");
@@ -15,6 +15,10 @@ app.use(postsRoute);
 app.use(adminRoute);
 app.use(userRoute);
 
-app.listen(process.env.PORT || PORT, () => {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(PORT, () => {
   console.log(`---- APP LISTENING AT PORT ${PORT} ------`);
 });
