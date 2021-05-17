7fa4005ba89507ff975567bd08cacd90771a99f7
Netanel Vaknin <emailofnetanel@gmail.com>
2021-05-17 22:26:02 +0300

    Change posts schema

--- models/posts.js
+++ models/posts.js
@@ -5,7 +5,7 @@ const PostSchema = new mongoose.Schema(
     title: { type: String, required: true },
     content: { type: String, required: true },
     createdAt: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
   },
   { collection: "my-posts" }
 );
